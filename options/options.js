2394eefef47c10161053a8e71b3152ac21e0abc6
Crowdbotics <team@crowdbotics.com>
2023-04-12 07:31:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "linxleague-golf-app-39856",
+  url: "https://linxleague-golf-app-39856.botics.co",
+  api: "https://linxleague-golf-app-39856.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
